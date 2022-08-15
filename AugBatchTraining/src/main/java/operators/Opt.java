a914461911ef35e810bc43e79d3d356c413b7316
manish <manishymca19@gmail.com>
2022-08-15 18:24:45 +0530

    Second commit - 15/8/22 opeartors

---
+++ AugBatchTraining/src/main/java/operators/Opt.java
@@ -0,0 +1,30 @@
+package operators;
+
+
+/**
+ * We will discuss all the operators in java..
+ * 
+ * @author Manish
+ *
+ */
+public class Opt {	
+	
+	/**
+	 * Arithmetic operators 
+	 * 
+	 */
+	
+	
+	 
+	 public static void main(String[] args) {
+			/*
+			 * int x=10; int y=20;
+			 * 
+			 * System.out.println(x+y); System.out.println(x-y); System.out.println(x*y);
+			 * System.out.println(x/y); System.out.println(x%y);
+			 */
+		 
+	}
+	
+
+}
